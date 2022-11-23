c5f68f7a0ad5ab434f49ed059060049f2bda226f
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-23 14:11:46 -0300

    migrating file

--- db/migrate/20221123003500_create_products.rb
+++ db/migrate/20221123003500_create_products.rb
@@ -1,33 +1,45 @@
+  # rubocop:disable Metrics/CyclomaticComplexity
+
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :photo # as photo should be a link to an image not a file
-      t.text :bio
-      t.integer :posts_counter
-      t.timestamps # this will crate uopdated_at and created_at columns
-    end
-
-    create_table :posts do |t|
-      t.string :title
+      t.integer :post_id 
+      t.integer :author_id 
+      t.character varying :title
       t.text :text
+      t.date :created_at
+      t.date :updated_at
       t.integer :comments_counter
-      t.integer :likes_counter
-      t.timestamps # this will crate uopdated_at and created_at columns
-      t.references :author, foreign_key: { to_table: 'users' } # this will create author_id and a corresponding index  (but students can also add column and index in separate commands)
+      t.integer :likes_count
+      t.timestamps
     end
-
-    create_table :comments do |t|
-      t.text :text
-      t.timestamps # this will crate uopdated_at and created_at columns
-      t.references :author, foreign_key: { to_table: 'users' } # this will create author_id and a corresponding index
-      t.references :post, foreign_key: true # this will create post_id and a corresponding index (but students can also add column and index in separate commands)
-    end
-
     create_table :likes do |t|
-      t.timestamps # this will crate uopdated_at and created_at columns
-      t.references :author, foreign_key: { to_table: 'users' } # this will create author_id and a corresponding index
-      t.references :post, foreign_key: true # this will create post_id and a corresponding index (but students can also add column and index in separate commands)
+      t.integer :likes_id
+      t.integer :author_id
+      t.integer :post_id
+      t.date :created_at
+      t.date :updated_at
+    end
+    create_table :comments do |t|
+      t.integer :comment_id
+      t.integer :author_id
+      t.integer :post_id
+      t.text :text
+      t.date :updated_at
+      t.date :created_at
+    end
+    create_table :posts do |t|
+      t.integer :post_id
+      t.integer :author_id
+      t.character varying :title
+      t.text :text
+      t.date :created_at
+      t.date :updated_at
+      t.integer :comments_counter
+      t.integer :likes_count
     end
   end
 end
+
+  # rubocop:enable Metrics/CyclomaticComplexity
+
