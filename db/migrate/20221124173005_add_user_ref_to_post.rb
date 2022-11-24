35b21c3c3b47934499035c396c12adb32fccee5e
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-24 15:51:56 -0300

    script permission

--- db/migrate/20221124173005_add_user_ref_to_post.rb
+++ db/migrate/20221124173005_add_user_ref_to_post.rb
@@ -1,5 +1,5 @@
 class AddUserRefToPost < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :author, null: false, foreign_key: true, foreign_key: { to_table: 'users' }
+    add_reference :posts, :author, null: false, primary_key: true, foreign_key: { to_table: 'users' }
   end
 end
