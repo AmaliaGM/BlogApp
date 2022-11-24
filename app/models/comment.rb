d835249ccfdbbcf831b9c72eb57de61178cf10ea
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-24 19:55:08 -0300

    linters fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id,
-  belongs_to :post, counter_cache: :comments_counter, foreign_key: :post_id,
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, counter_cache: :comments_counter, foreign_key: :post_id
 end
