2398a052cf1a84b40112aea1433322ea8b04a2f1
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-25 08:21:06 -0300

    post word

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,6 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, counter_cache: :likes_counter
   def update_likes_counter
-    likes.increment!(:likes_counter)
+    post.increment!(:likes_counter)
   end
 end
