d01015ecebca3978693c45f292d3090a8fea12c7
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-25 07:37:14 -0300

    count likes, comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, counter_cache: :comments_counter, foreign_key: :post_id
-  post.increment!(:comments_counter)
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, counter_cache: :likes_counter
+  def update_likes_counter
+    likes.increment!(:likes_counter)
+  end
 end
