1a11ef404232ff64f964cc32eb3345476f268aa2
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-11-30 17:32:58 -0300

    erase pending tests

--- spec/requests/blog_app_spec.rb
+++ spec/requests/blog_app_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'BlogApps', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+   # pending "add some examples (or delete) #{__FILE__}"
   end
 end
