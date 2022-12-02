575c0a68c69c1044987c6fb6c1d78310d520fc49
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-02 09:05:19 -0300

    fix post index, fix post number, fix add new link, fix image

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,11 @@
 class PostController < ApplicationController
   def index
+    @users = User.all
     @posts = Post.all
   end
 
   def show
+    @user = User.find(params[:id])
     @post = Post.find(params[:id])
   end
 
